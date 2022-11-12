8472380e86f5fee8b9639d60bcac3baf3e38723f
Jakub <jkbmielewczyk@gmail.com>
2022-11-12 10:35:28 +0100

    Updates

---
+++ precache-manifest.4f284d351aaf9c185c8b74be30f9ac91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0855e6210a31f2da9e7",
+    "url": "/reactRouter-testing/static/css/main.32f54097.chunk.css"
+  },
+  {
+    "revision": "a0855e6210a31f2da9e7",
+    "url": "/reactRouter-testing/static/js/main.e00c9f39.chunk.js"
+  },
+  {
+    "revision": "f379b90454b782dc96fb",
+    "url": "/reactRouter-testing/static/js/runtime~main.a2b91b3b.js"
+  },
+  {
+    "revision": "9e4a96facc4fe5e9ddb6",
+    "url": "/reactRouter-testing/static/js/2.d97d972e.chunk.js"
+  },
+  {
+    "revision": "6555142f1108211925170b5566419256",
+    "url": "/reactRouter-testing/static/media/avenue.6555142f.jpg"
+  },
+  {
+    "revision": "0bc72dd25ec0e5d8e55578479d8dc15e",
+    "url": "/reactRouter-testing/static/media/highway.0bc72dd2.jpg"
+  },
+  {
+    "revision": "f55a253594bdbfa7fef7931e3dbfb269",
+    "url": "/reactRouter-testing/static/media/railroad.f55a2535.jpg"
+  },
+  {
+    "revision": "8fa2122040d1b517bc40b69c9bd27b1f",
+    "url": "/reactRouter-testing/static/media/road.8fa21220.jpg"
+  },
+  {
+    "revision": "48c2ead53fd263ba9bdeee3490ecb465",
+    "url": "/reactRouter-testing/index.html"
+  }
+];
